a1918fbc2bb6e333ec09fd6d6a0e1653d2b75cf9
Magnitaizer <magnitaizer@gmx.com>
2022-03-23 12:53:44 +0300

    Fixed getty1 correct functioning now

--- plex.sh
+++ plex.sh
@@ -7,14 +7,14 @@ chown nuc /home/nuc/.xinitrc
 echo 'if [[ ! ${DISPLAY} && ${XDG_VTNR} == 1 ]]; then' >> /home/nuc/.bash_profile
 echo '     exec startx' >> /home/nuc/.bash_profile
 echo 'fi' >> /home/nuc/.bash_profile
-sed -i 's+-/sbin/agetty -o '-p -- \\u' --noclear - $TERM +-/sbin/agetty -a USERNAME %I $TERM+g' /etc/systemd/system/getty.target.wants/getty@tty1.service
+sed -i '38s+.*ExecStart.*+ExecStart=-/sbin/agetty -a nuc %I $TERM+' /etc/systemd/system/getty.target.wants/getty@tty1.service
 sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl disable display-manager.service
 passwd -d nuc
-pacman -Fy && pacman -Syu  && pacman -S base-devel xorg-xinit xorg
-git clone https://aur.archlinux.org/trizen.git /home/nuc
-chown nuc /home/nuc/trizen
-cd /home/nuc/trizen
+pacman -S base-devel xorg-xinit xorg git
+#git clone https://aur.archlinux.org/trizen.git /home/nuc
+#chown nuc /home/nuc/trizen
+#cd /home/nuc/trizen
 #makepkg -sri
 #trizen -S plex-media-player
