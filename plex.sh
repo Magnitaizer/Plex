eb5e124c76dfb8182adcecd6727bd6ad3dff5e28
Magnitaizer <magnitaizer@gmx.com>
2022-03-23 14:44:05 +0300

    Update plex.sh

--- plex.sh
+++ plex.sh
@@ -12,7 +12,7 @@ sudo sed -i "38s+.*ExecStart.*+ExecStart=-/sbin/agetty -a "$USER" %I $TERM+" /et
 sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo systemctl disable display-manager.service
-sudo pacman -S base-devel xorg-xinit xorg git --noconfirm
+sudo pacman -S base-devel xorg-xinit xorg git cmake rhash jsoncpp desktop-file-utils --noconfirm
 git clone https://aur.archlinux.org/trizen.git /home/$USER/trizen
 cd /home/$USER/trizen
 makepkg -sri
