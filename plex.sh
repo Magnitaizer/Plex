2c8ea0ac260b03e1874b1a15808a42b208afcfd8
Magnitaizer <magnitaizer@gmx.com>
2022-03-25 20:57:30 +0300

    ru locale added

--- plex.sh
+++ plex.sh
@@ -32,6 +32,8 @@ sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+localectl set-locale ru_RU.UTF-8
+
 sudo systemctl disable display-manager.service
 
 sudo systemctl mask systemd-udev-settle
