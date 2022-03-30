f45f72aac9619b7ba6e0653675fc5bc0a59645d7
Magnitaizer <magnitaizer@gmx.com>
2022-03-30 12:44:44 +0300

    Added soundcard hdmi select

--- plex.sh
+++ plex.sh
@@ -46,6 +46,8 @@ sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+pactl set-card-profile 0 output:hdmi-stereo
+
 sudo systemctl disable display-manager.service
 
 sudo systemctl mask systemd-udev-settle
