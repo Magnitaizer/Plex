b0c35ccd48642931633dec89eb53cac8b6f64fd8
Magnitaizer <magnitaizer@gmx.com>
2022-04-06 10:15:04 +0300

    removed sleep

--- Lplex.sh
+++ Lplex.sh
@@ -39,6 +39,10 @@ sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+xset -dpms
+
+xset s off
+
 pactl set-card-profile 0 output:hdmi-stereo
 
 sudo systemctl disable display-manager.service
