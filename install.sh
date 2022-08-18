fade93f161cb729dbebef2c92f530da1a2c99261
Magnitaizer <magnitaizer@gmx.com>
2022-08-18 17:32:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,47 +1,21 @@
 #!/bin/sh
 
-sudo passwd -d $USER
-sudo pacman -Syu  --noconfirm
-sudo pacman -S base-devel xorg-xinit python-pip --noconfirm
-
 git clone https://github.com/Magnitaizer/Plex.git /home/$USER/Plex
 
-paru -S plex-media-player --noconfirm
+sudo pacman -S base-devel python-pip flatpak unclutter libva-intel-driver --noconfirm
+sudo pacman -R orca --noconfirm
+sudo pacman -Syu  --noconfirm
 
 pip install pynput
 
-if grep -q 'exec plexmediaplayer' "/home/$USER/.xinitrc"; then
-  echo 'skipping this part...'
-else
-  echo ' ' >> /home/$USER/.xinitrc
-  echo 'exec python3 Plex/local_control.py &' >> /home/$USER/.xinitrc
-  echo 'xset s off -dpms' >> /home/$USER/.xinitrc
-  echo 'exec plexmediaplayer' >> /home/$USER/.xinitrc
-fi
-
-if grep -q 'exec startx' "/home/$USER/.bash_profile"; then
-  echo 'skipping this part...'
-else
-  echo ' ' >> /home/$USER/.bash_profile
-  echo 'if [[ ! ${DISPLAY} && ${XDG_VTNR} == 1 ]]; then' >> /home/$USER/.bash_profile
-  echo '     exec startx' >> /home/$USER/.bash_profile
-  echo 'fi' >> /home/$USER/.bash_profile
-fi
-
-if grep -q 'defaults.pcm.card 0' "/etc/asound.conf"; then
-  echo 'skipping this part...'
-else
-  sudo echo ' ' | sudo tee -a /etc/asound.conf
-  sudo echo 'defaults.pcm.card 0' | sudo tee -a /etc/asound.conf
-  sudo echo 'defaults.pcm.device 3' | sudo tee -a /etc/asound.conf
-  sudo echo 'defaults.ctl.card 0' | sudo tee -a /etc/asound.conf
-fi
-
-sudo sed -i --follow-symlinks "38s+.*ExecStart.*+ExecStart=-/sbin/agetty -a "$USER' %I $TERM+' /etc/systemd/system/getty.target.wants/getty@tty1.service
+flatpak install flathub tv.plex.PlexHTPC
 
 sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-sudo systemctl disable display-manager.service
+sudo systemctl enable sshd
+
+mv /home/$USER/Plex/config /home/$USER/.config/i3/config
+
 
