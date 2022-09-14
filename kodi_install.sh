622c5c0766b19b6426f4e4fa46a00d0feeb1edd9
Magnitaizer <86266935+Magnitaizer@users.noreply.github.com>
2022-09-14 17:20:29 +0300

    Update kodi_install.sh

--- kodi_install.sh
+++ kodi_install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 sudo passwd -d $USER
-sudo apt update &&  sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt install base-devel kodi -y
+sudo pacman -Syu  --noconfirm
+sudo pacman -S base-devel xorg-xinit python-pip kodi --noconfirm
 
-git clone https://github.com/Magnitaizer/Plex.git /home/$USER/Kodi
+git clone https://github.com/Magnitaizer/Kodi.git /home/$USER/Kodi
 
 pip install pynput
 
-if grep -q 'exec kodi' "/home/$USER/.xinitrc"; then
+if grep -q 'exec plexmediaplayer' "/home/$USER/.xinitrc"; then
   echo 'skipping this part...'
 else
   echo ' ' >> /home/$USER/.xinitrc
@@ -26,6 +26,19 @@ else
   echo 'fi' >> /home/$USER/.bash_profile
 fi
 
+if grep -q 'defaults.pcm.card 0' "/etc/asound.conf"; then
+  echo 'skipping this part...'
+else
+  sudo echo ' ' | sudo tee -a /etc/asound.conf
+  sudo echo 'defaults.pcm.card 0' | sudo tee -a /etc/asound.conf
+  sudo echo 'defaults.pcm.device 3' | sudo tee -a /etc/asound.conf
+  sudo echo 'defaults.ctl.card 0' | sudo tee -a /etc/asound.conf
+fi
+
 sudo sed -i --follow-symlinks "38s+.*ExecStart.*+ExecStart=-/sbin/agetty -a "$USER' %I $TERM+' /etc/systemd/system/getty.target.wants/getty@tty1.service
+
+sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=0+g' /etc/default/grub
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 sudo systemctl disable display-manager.service
